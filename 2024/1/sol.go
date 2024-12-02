fb26e399e31197d9bd06775fe9afaaef3b44989b
datajan <jmhoie@protonmail.com>
2024-12-02 15:16:40 +0100

    refactor

--- 2024/1/sol.go
+++ 2024/1/sol.go
@@ -9,25 +9,35 @@ import (
 	"strings"
 )
 
-func parseFile(file string) ([]int, []int) {
-	f, err := os.Open(file)
-	if err != nil {
-		panic(fmt.Errorf("Error opening file: %w", err))
-	}
+func main() {
+	s1, s2 := parseFile("input.txt")
+	totalDistance, similarityScore := sumShortestDistance(s1, s2), calcSimilarityScore(s1, s2)
+	fmt.Printf("Answer 1: %v\nAnswer 2: %v\n", totalDistance, similarityScore)
+}
 
-	scanner := bufio.NewScanner(f)
-	var l1, l2 []int
-	for scanner.Scan() {
-		line := scanner.Text()
-		ss := strings.Fields(line)
-		leftNum, _ := strconv.Atoi(ss[0])
-		rightNum, _ := strconv.Atoi(ss[1])
-		l1 = append(l1, leftNum)
-		l2 = append(l2, rightNum)
+func sumShortestDistance(s1, s2 []int) int {
+	sum := 0
+	for i := 0; i < len(s1); i++ {
+		sum += abs(s1[i] - s2[i])
 	}
-	slices.Sort(l1)
-	slices.Sort(l2)
-	return l1, l2
+	return sum
+}
+
+func calcSimilarityScore(s1, s2 []int) int {
+	sum := 0
+	for i := 0; i < len(s1); i++ {
+		count := 0
+		for _, v := range s2 {
+			if v > s1[i] {
+				break
+			}
+			if v == s1[i] {
+				count++
+			}
+		}
+		sum += s1[i] * count
+	}
+	return sum
 }
 
 func abs(x int) int {
@@ -37,33 +47,24 @@ func abs(x int) int {
 	return x
 }
 
-func sumShortestDistance(l1, l2 []int) int {
-	sum := 0
-	for i := 0; i < len(l1); i++ {
-		sum += abs(l1[i] - l2[i])
+func parseFile(path string) ([]int, []int) {
+	file, err := os.Open(path)
+	if err != nil {
+		panic(fmt.Errorf("Error opening file: %w", err))
 	}
-	return sum
-}
+	defer file.Close()
 
-func calcSimilarityScore(l1, l2 []int) int {
-	sum := 0
-	for i := 0; i < len(l1); i++ {
-		count := 0
-		for _, v := range l2 {
-			if v > l1[i] {
-				break
-			}
-			if v == l1[i] {
-				count++
-			}
-		}
-		sum += l1[i] * count
+	scanner := bufio.NewScanner(file)
+	var s1, s2 []int
+	for scanner.Scan() {
+		line := scanner.Text()
+		ss := strings.Fields(line)
+		leftNum, _ := strconv.Atoi(ss[0])
+		rightNum, _ := strconv.Atoi(ss[1])
+		s1 = append(s1, leftNum)
+		s2 = append(s2, rightNum)
 	}
-	return sum
-}
-
-func main() {
-	l1, l2 := parseFile("input.txt")
-	ans1, ans2 := sumShortestDistance(l1, l2), calcSimilarityScore(l1, l2)
-	fmt.Printf("Answer 1: %v\nAnswer 2: %v\n", ans1, ans2)
+	slices.Sort(s1)
+	slices.Sort(s2)
+	return s1, s2
 }
