676227440f038c87a974e08ab10b4cc7be5f85dc
datajan <jmhoie@protonmail.com>
2024-12-03 20:41:36 +0100

    refactor + comments

--- 2024/3/sol.zig
+++ 2024/3/sol.zig
@@ -1,41 +1,44 @@
 const std = @import("std");
 const input = @embedFile("input.txt");
 
+const Factors = struct {
+    f1: u32, // first factor
+    f2: u32, // second factor
+    do: bool, // bool flag for task 2
+};
+
 pub fn main() !void {
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     defer arena.deinit();
     const allocator = arena.allocator();
 
-    const factor_pairs = try parseFile(allocator);
-    const sums = sumProducts(factor_pairs.factors, factor_pairs.factors_cond);
+    const factors = try parseFile(allocator);
+    const sums = sumProducts(factors);
     try std.io.getStdOut().writer().print("Answer 1: {d}\nAnswer 2: {d}\n", .{ sums.sum, sums.cond });
 }
 
-fn sumProducts(pairs: std.ArrayList(struct { u32, u32 }), pairs_cond: std.ArrayList(struct { u32, u32 })) struct { sum: u32, cond: u32 } {
+fn sumProducts(pairs: std.ArrayList(Factors)) struct { sum: u32, cond: u32 } {
     var sum: u32 = 0;
     var sum_cond: u32 = 0;
 
-    for (pairs.items) |factors| {
-        sum += factors[0] * factors[1];
+    for (pairs.items) |f| {
+        sum += f.f1 * f.f2;
+        if (f.do) sum_cond += f.f1 * f.f2;
     }
-    for (pairs_cond.items) |factors| {
-        sum_cond += factors[0] * factors[1];
-    }
-
     return .{ .sum = sum, .cond = sum_cond };
 }
 
-fn parseFile(allocator: std.mem.Allocator) !struct { factors: std.ArrayList(struct { u32, u32 }), factors_cond: std.ArrayList(struct { u32, u32 }) } {
-    // list of pairs of factors -> [(2, 4), (5, 5), (11, 8), (8, 5), ..]
-    var factors = std.ArrayList(struct { u32, u32 }).init(allocator);
-    var factors_cond = std.ArrayList(struct { u32, u32 }).init(allocator); // for task 2
+fn parseFile(allocator: std.mem.Allocator) !std.ArrayList(Factors) {
+    // list that contains two factors + a bool flag for task 2
+    var factors = std.ArrayList(Factors).init(allocator);
     const mul_str_prefix = "mul(";
 
-    var do = true;
+    var do = true; // flag for task 2
     var f_iter = std.mem.tokenizeScalar(u8, input, '\n');
     while (f_iter.next()) |line| {
         var i: u16 = 0;
-        blk: while (i < line.len) : (i += 1) {
+        while (i < line.len) : (i += 1) {
+            // check if new do flag
             if (std.mem.startsWith(u8, line[i..], "don't()")) {
                 do = false;
                 i += 7; // skip over the characters in "don't()"
@@ -43,26 +46,27 @@ fn parseFile(allocator: std.mem.Allocator) !struct { factors: std.ArrayList(stru
                 do = true;
                 i += 4; // skip over the characters in "do()"
             }
-
+            // check if substr starts with "mul("
             if (std.mem.startsWith(u8, line[i..], mul_str_prefix)) {
+                // check for numbers separated by a single comma that ends with a ')', e.g. "231,970)"
                 for (line[i + mul_str_prefix.len ..], 0..) |c, j| {
-                    if (c != ',' and c != ')' and !std.ascii.isDigit(c)) continue :blk;
+                    if (c != ',' and c != ')' and !std.ascii.isDigit(c)) break;
                     if (c == ')') {
+                        // num_str contains the substring of two numbers separated by a comma, e.g. "231,970"
                         const num_str = line[i + mul_str_prefix.len .. i + mul_str_prefix.len + j];
+
+                        // split string on ',' and extract the two numbers into f1 and f2. "231,970" ==> f1=231; f2=970;
                         var num_iter = std.mem.tokenizeScalar(u8, num_str, ',');
-
-                        const a = try std.fmt.parseInt(u32, num_iter.next().?, 10);
-                        const b = try std.fmt.parseInt(u32, num_iter.next().?, 10);
-
-                        try factors.append(.{ a, b });
-                        if (do) try factors_cond.append(.{ a, b });
+                        const f1 = try std.fmt.parseInt(u32, num_iter.next().?, 10);
+                        const f2 = try std.fmt.parseInt(u32, num_iter.next().?, 10);
+                        try factors.append(.{ .f1 = f1, .f2 = f2, .do = do }); // instantiate Factors struct and append to list
 
                         i += @intCast(4 + j); // skip over the characters in the product string
-                        continue :blk;
+                        break;
                     }
                 }
             }
         }
     }
-    return .{ .factors = factors, .factors_cond = factors_cond };
+    return factors;
 }
